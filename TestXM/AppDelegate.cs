911db67c123b2e2e7470c11c50c2f1627bfe43bc
Dima <dmitriy.kirakosyan@gmail.com>
2022-02-22 00:43:10 +0300

    initial commit

---
+++ TestXM/AppDelegate.cs
@@ -0,0 +1,27 @@
+﻿using AppKit;
+using Foundation;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace TestXM
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : NSApplicationDelegate
+    {
+        public AppDelegate()
+        {
+        }
+
+        public override void DidFinishLaunching(NSNotification notification)
+        {
+            AppCenter.LogLevel = LogLevel.Verbose;
+            AppCenter.Start("empty", typeof(Analytics), typeof(Crashes));
+        }
+
+        public override void WillTerminate(NSNotification notification)
+        {
+            // Insert code here to tear down your application
+        }
+    }
+}
---
+++ TestXM/ViewController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using AppKit;
+using Foundation;
+using Microsoft.AppCenter.Analytics;
+
+namespace TestXM
+{
+    public partial class ViewController : NSViewController
+    {
+        int likes = 0;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Do any additional setup after loading the view.
+        }
+
+        partial void OnButtonClick(NSObject sender)
+        {
+            likes += 1;
+            Analytics.TrackEvent("button clicked", new Dictionary<string, string> { ["likes"] = $"{likes}" });
+
+            var button = sender as NSButton;
+            button.Title = $"👍 {likes}";
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
